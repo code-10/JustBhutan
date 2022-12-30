7feb0f31033fdd122f57e15a9850ecd9803f5cc7
EagleCoder404 <harsha.jediknight@gmail.com>
2022-12-31 01:22:36 +0530

    init react+vite project

---
+++ frontend/src/pages/landing_page/landing_page.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return(
+        <div>
+                lolol
+        </div>
+    )
+}
---
+++ frontend/src/router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import Layout from "./layout"
+import LandingPage from "./pages/landing_page/landing_page"
+
+export default createBrowserRouter([
+    {
+        path:'/',
+        element: <Layout />,
+        children: [
+            { index:true, element:<LandingPage />}
+        ],
+    }
+])
