6eb956530589cd5749d3c979a1c40d963dc2552d
Manoj Kumar A <manoj.code10@gmail.com>
2022-12-30 19:26:42 +0000

    added static settings

--- justbhutan/settings.py
+++ justbhutan/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'react_build', 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
