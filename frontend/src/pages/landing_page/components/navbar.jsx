99337f09cca9a3f5a1ec22d0effb92427f0376ce
EagleCoder404 <harsha.jediknight@gmail.com>
2022-12-31 01:50:44 +0530

    created basic navbar component

---
+++ frontend/src/pages/landing_page/components/navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "../../../assets/logo.svg"
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-light bg-primary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={logo}
+            alt="Just Bhutan Logo"
+            width="113"
+            className="d-inline-block align-text-top"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+}
