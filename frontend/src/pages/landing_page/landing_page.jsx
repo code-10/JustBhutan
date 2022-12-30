9b88caf6edf7d824ac3213f349c518a7708e41b8
EagleCoder404 <harsha.jediknight@gmail.com>
2022-12-31 01:50:03 +0530

    added navbar component

--- frontend/src/pages/landing_page/landing_page.jsx
+++ frontend/src/pages/landing_page/landing_page.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar";
+
 export default function LandingPage() {
     return(
         <div>
-                lolol
+            <Navbar/>
         </div>
     )
 }
