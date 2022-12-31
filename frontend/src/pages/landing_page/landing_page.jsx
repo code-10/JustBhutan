f43ad8aec92f23ba80a169da6164036f24fe7f50
EagleCoder404 <harsha.jediknight@gmail.com>
2022-12-31 17:05:30 +0530

    AaaaOooo

--- frontend/src/pages/landing_page/landing_page.jsx
+++ frontend/src/pages/landing_page/landing_page.jsx
@@ -1,9 +1,17 @@
-import Navbar from "./components/navbar";
+import ForestSection from "./components/forest_section";
+import HeaderSection from "./components/header_section";
+import NavbarSection from "./components/navbar_section";
+import PlantSection from "./components/plant_section";
 
 export default function LandingPage() {
     return(
-        <div>
-            <Navbar/>
+        <div >
+            <NavbarSection/>
+            <div className="vstack gap-5">
+                <HeaderSection/>
+                <PlantSection/>
+                <ForestSection/>
+            </div>
         </div>
     )
 }
