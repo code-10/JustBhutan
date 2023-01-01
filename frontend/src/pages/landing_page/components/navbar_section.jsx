10cce3aa06eccc9d82d9e21accb27be4ea5ba262
EagleCoder404 <harsha.jediknight@gmail.com>
2023-01-01 14:45:19 +0530

    added navbar # elements along with some padding

--- frontend/src/pages/landing_page/components/navbar_section.jsx
+++ frontend/src/pages/landing_page/components/navbar_section.jsx
@@ -2,16 +2,32 @@ import logo from "../../../assets/logo.svg"
 
 export default function NavbarSection() {
   return (
-    <nav className="navbar navbar-light bg-primary">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <div className="container-fluid px-5 py-3">
         <a className="navbar-brand" href="#">
           <img
             src={logo}
             alt="Just Bhutan Logo"
-            width="113"
+            width="138"
             className="d-inline-block align-text-top"
           />
         </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav fw-semibold">
+            <li className="nav-item">
+              <a className="nav-link active text-light" aria-current="page" href="#">The Problem</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active text-light" aria-current="page" href="#">The Solution</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active text-light" aria-current="page" href="#">Sign Up!</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
