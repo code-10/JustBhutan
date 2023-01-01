bd27d4d3112e967290d549713fdf9f3b38576a25
code-10 <manoj.code10@gmail.com>
2023-01-01 20:13:38 +0530

    forgot to add views and models to 1b1da0c12b8e01cecf4644ae8c1e6289e84ea69a

--- justbhutan/survey/models.py
+++ justbhutan/survey/models.py
@@ -5,10 +5,18 @@ from django.db.utils import OperationalError
 from justbhutan.utility.response import convertListToJson
 from rest_framework import status
 
-def AddSurvey():
+def AddSurvey(survey_request):
     db_connection = connections['default']
     try:
         cursor = db_connection.cursor()
-        cursor.execute('''select * from product''')
+        cursor.execute('''insert into survey(name, email_id, phone_number, pincode, city_id)
+            values(%s,%s,%s,%s,%s)''', [
+                survey_request['name'], 
+                survey_request['email_id'], 
+                survey_request['phone_number'], 
+                survey_request['pincode'], 
+                survey_request['city_id']
+            ])
+        return "survey added successfully"
     except OperationalError as e:
         JsonResponse("There is something wrong with the database connection", e)
--- justbhutan/survey/views.py
+++ justbhutan/survey/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 from . import models
 
 @csrf_exempt
 def AddSurvey(request):
     if request.method == 'POST':
         try:
-            
-            #result = models.AddSurvey()
-            return JsonResponse("result",status=status.HTTP_200_OK,safe=False)
+            survey_request = JSONParser().parse(request)
+            result = models.AddSurvey(survey_request)
+            return JsonResponse(result,status=status.HTTP_200_OK,safe=False)
         except Exception as e:
             JsonResponse("Something went wrong while adding a survey", e)
