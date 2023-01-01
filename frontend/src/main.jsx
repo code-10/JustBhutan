b87440576b732b61e494cdda144e482c81ad745c
EagleCoder404 <harsha.jediknight@gmail.com>
2023-01-01 14:44:55 +0530

    imported bootstrap js stuff into main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import "./custom.scss"
+import * as bootstrap from 'bootstrap'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
