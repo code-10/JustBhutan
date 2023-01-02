3c908b8919519e9bb2f1bfa59431d160c92fe407
EagleCoder404 <harsha.jediknight@gmail.com>
2023-01-02 17:02:02 +0530

    proxy server added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target:"https://just-bhutan.herokuapp.com",
+        changeOrigin: true,
+        secure:false,
+      }
+    }
+  }
 })
