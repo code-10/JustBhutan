79465bad0bf7a1e97be1bfb77ef13276d1097b72
code-10 <manoj.code10@gmail.com>
2023-01-02 16:47:10 +0530

    added product_ids and stashing toexcel

--- justbhutan/survey/models.py
+++ justbhutan/survey/models.py
@@ -1,22 +1,28 @@
+from http.client import HTTPResponse
 from django.db import models
 from django.http import JsonResponse
 from django.db import connections
 from django.db.utils import OperationalError
 from justbhutan.utility.response import convertListToJson
 import xlsxwriter
+try:
+    from StringIO import BytesIO
+except ImportError:
+    from io import BytesIO
 # from justbhutan.utility.excel import ExportDataToExcel
 
 def AddSurvey(survey_request):
     db_connection = connections['default']
     try:
         cursor = db_connection.cursor()
-        cursor.execute('''insert into survey(name, email_id, phone_number, pincode, city_id)
-            values(%s,%s,%s,%s,%s)''', [
+        cursor.execute('''insert into survey(name, email_id, phone_number, pincode, city_id, product_ids)
+            values(%s,%s,%s,%s,%s,%s)''', [
                 survey_request['name'], 
                 survey_request['email_id'], 
                 survey_request['phone_number'], 
                 survey_request['pincode'], 
-                survey_request['city_id']
+                survey_request['city_id'],
+                survey_request['product_ids']
             ])
         return "survey added successfully"
     except OperationalError as e:
@@ -28,21 +34,15 @@ def SurveyToExcel():
         cursor = db_connection.cursor()
         cursor.execute('''select * from survey''')
         data = cursor.fetchall()
-        print(data)
 
-        workbook = xlsxwriter.Workbook('survey.xlsx')
+        output = BytesIO()
+
+        workbook = xlsxwriter.Workbook(output)
         worksheet = workbook.add_worksheet()
         
         header_data = ("ID", "Name", "Email ID", "Phone Number", "Pincode", "City ID")
 
-        header_format = workbook.add_format({'bold': True,
-                                     'bottom': 2,
-                                     'bg_color': '#57C377'})
-
         data.insert(0,header_data)
-
-        # for col_num, data in enumerate(header_data):
-        #     worksheet.write(header_format)
 
         for row, line in enumerate(data):
             for col, cell in enumerate(line):
@@ -50,6 +50,10 @@ def SurveyToExcel():
 
         workbook.close()
 
-        return "survey exported successfully"
+        output.seek(0)
+        response = HTTPResponse(output.read(), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        response['Content-Disposition'] = "attachment; filename=survey.xlsx"
+
+        return response
     except OperationalError as e:
         JsonResponse("There is something wrong with the database connection", e)
