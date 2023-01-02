1d4a915aec19162600ee0e00bd0cd013de7ae5a6
EagleCoder404 <harsha.jediknight@gmail.com>
2023-01-02 17:11:17 +0530

    updated proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"https://just-bhutan.herokuapp.com",
+        target:"http://localhost:8000",
         changeOrigin: true,
         secure:false,
       }
