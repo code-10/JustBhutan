c2e97e679ca300c932e9279c727567e6644165a0
code-10 <manoj.code10@gmail.com>
2023-01-05 02:06:58 +0530

    w-100 for forest section

--- frontend/src/pages/landing_page/components/forest_section.jsx
+++ frontend/src/pages/landing_page/components/forest_section.jsx
@@ -5,7 +5,7 @@ export default function ForestSection () {
         <div className="text-center" id="forest">
             <div className="position-relative">
                 <img src={forest_image} className="ratio ratio-16x9 forest-image" alt="forest image"/>
-                <div className="position-absolute py-3 py-md-5 w-75 top-50 start-50 text-center translate-middle text-light translucent-black-background">
+                <div className="position-absolute py-3 py-md-5 w-100 top-50 start-50 text-center translate-middle text-light translucent-black-background">
                     <h3 className="text-heading mb-3">
                         Our Solution: Herbal Teas From Bhutan.
                     </h3>
